2800ffe9dec639d9a98159e764c436c121342540
zeratheuz <zeratheuz@gmail.com>
2025-12-28 16:47:26 -0300

    Update JS

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,5 +1,5 @@
 export async function getWeather(place) {
-  const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/joao+pessoa/today?unitGroup=metric&keyy`=UBKPCGKX7RTGV7W8ACP2G2JMW")
+  const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/curitiba/today?unitGroup=metric&keyy`=UBKPCGKX7RTGV7W8ACP2G2JMW")
   const weatherData = await response.json()
   console.log(weatherData.days[0].temp)
 }
