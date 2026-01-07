18ada6e25ccbab000e9af3ab9f3d243125d74819
zeratheuz <zeratheuz@gmail.com>
2026-01-07 16:49:39 -0300

    Update CSS and JS

--- src/displayWeather.js
+++ src/displayWeather.js
@@ -2,5 +2,5 @@ import { searchWeather } from "./searchWeather.js";
 
 export async function displayWeather() {
   const container = document.querySelector("#container")
-  container.innerHTML = await searchWeather()
+  container.innerHTML = await searchWeather() + "°C"
 }
--- src/getWeather.js
+++ src/getWeather.js
@@ -1,7 +1,8 @@
-export async function getWeather(place = "United+States") {
+export async function getWeather(place = "Texas") {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/today?unitGroup=metric&key=UBKPCGKX7RTGV7W8ACP2G2JMW`
   const response = await fetch(url)
   const weatherData = await response.json()
+  console.log(weatherData)
   const weatherToday = weatherData.days[0].temp
   return weatherToday
 }
