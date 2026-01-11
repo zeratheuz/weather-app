d793e66c7d65d675876d985ab162be428c59d8e6
zeratheuz <zeratheuz@gmail.com>
2026-01-11 17:46:38 -0300

    Update CSS and JS

--- src/searchWeather.js
+++ src/searchWeather.js
@@ -1,7 +1,8 @@
 import { getWeather } from "./getWeather.js";
 
 export async function searchWeather() {
-  const searchBarValue = document.querySelector("#searchBar").value.split(' ').join("+")
+  const searchBarValue = document.querySelector("#searchBar").value
+  .split(' ').map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" ")
   if (searchBarValue == "") {
     return await getWeather()
   }
