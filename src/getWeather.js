80515216d1affb8fa130d20b1e28bd132a181646
zeratheuz <zeratheuz@gmail.com>
2026-01-15 17:02:36 -0300

    Update CSS and JS

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,5 +1,6 @@
 export async function getWeather(unit = "metric", place = "Texas") {
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=${unit}&key=UBKPCGKX7RTGV7W8ACP2G2JMW`
+  const API_KEY = "UBKPCGKX7RTGV7W8ACP2G2JMW"
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=${unit}&key=${API_KEY}`
   const response = await fetch(url)
   const weatherData = await response.json()
   return weatherData
