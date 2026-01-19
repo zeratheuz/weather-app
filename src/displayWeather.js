0bfe1afe94ad2711e6740d260d6c9905adee1522
zeratheuz <zeratheuz@gmail.com>
2026-01-19 16:47:44 -0300

    Update CSS and JS

--- src/displayWeather.js
+++ src/displayWeather.js
@@ -10,12 +10,12 @@ export async function displayWeather(data, unit) {
     <button id="unitBtn">${unit}</button>
     <p class="place">${data.address}</p>
     <div class="weather">
-    <img src="${iconURL}" class="icon" alt="${iconCode}">
     <p class="temp">${Math.round(data.days[0].temp)}°</p>
+    <img src="${iconURL}" class="icon" alt="${iconCode}">
     </div>
-    <p class="feelslike">Feels Like: ${Math.round(data.days[0].feelslike)}°</p>
-    <p class="conditions">${data.days[0].conditions}</p>
     <p class="humidity">Humidity: ${Math.round(data.days[0].humidity)}%</p>
+    <p class="conditions">${data.days[0].conditions}</p>
+    <p class="feelslike">Feels Like: ${Math.round(data.days[0].feelslike)}°</p>
     <p class="tempMaxAndMin">L: ${Math.round(data.days[0].tempmin)}° H: ${Math.round(data.days[0].tempmax)}°</p>
     `
 }
