ec8367d915c00cbf833daa56872db420d1c898bf
zeratheuz <zeratheuz@gmail.com>
2026-01-23 22:48:12 -0300

    Update HTML, CSS and JS

--- src/displayWeather.js
+++ src/displayWeather.js
@@ -3,10 +3,10 @@ import { getIcon } from "./getIcon.js"
 const container = document.querySelector("#container")
 
 export async function displayWeather(data, unit) {
-    const iconCode = data.days[0].icon
-    const iconURL = await getIcon(iconCode)
+  const iconCode = data.days[0].icon
+  const iconURL = await getIcon(iconCode)
 
-    container.innerHTML = `
+  container.innerHTML = `
     <button id="unitBtn">${unit}</button>
     <h2 class="place">${data.address}</h2>
     <div class="weather">
@@ -14,17 +14,29 @@ export async function displayWeather(data, unit) {
     <img src="${iconURL}" class="icon" alt="${iconCode}">
     </div>
     <div class="details">
+      
       <div class="detail">
         <img src="${await getIcon("raindrops")}">
         <p class="humidity">Humidity: ${Math.round(data.days[0].humidity)}%</p>
       </div>
-            <div class="detail">
+      
+        <div class="detail">
+          <img src="${await getIcon("thermometer")}">
+          <p class="feelslike">Feels Like: ${Math.round(data.days[0].feelslike)}°</p>
+        </div>
+      
+      <div class="detail">
         <img src="${iconURL}">
       <p class="conditions">${data.days[0].conditions}</p>
       </div>
-      
-      <p class="feelslike">Feels Like: ${Math.round(data.days[0].feelslike)}°</p>
-      <p class="tempMaxAndMin">L: ${Math.round(data.days[0].tempmin)}° H: ${Math.round(data.days[0].tempmax)}°</p>
-    </div>
+
+      <div class="detail">
+        <img src="${await getIcon("thermometer-colder")}">
+        <p class="tempMaxAndMin">L: ${Math.round(data.days[0].tempmin)}°</p>
+        <img src="${await getIcon("thermometer-warmer")}">
+        <p>H: ${Math.round(data.days[0].tempmax)}°</p>
+      </div>
+    
+      </div>
     `
 }
